4338a7ebdcf8f604d92e6632db5aa7cc0263ce44
Dev Kotak <60418882+dev856@users.noreply.github.com>
2025-01-19 14:22:41 -0500

    changes

---
+++ project2/trial.py
@@ -0,0 +1,11 @@
+#python program for finding even odd
+def isEven(n):
+    return (n % 2 == 0)
+
+
+if __name__ == "__main__":
+  n = 101
+  if isEven(n):
+      print("true")
+  else:
+      print("false")
---
+++ project2/trial2.py
@@ -0,0 +1,12 @@
+#python program for table printing
+def printTable(n, i=1):
+
+    if (i == 11):  # base case
+        return
+    print(n, "*", i, "=", n * i)
+    i += 1  
+    printTable(n, i)
+
+if __name__ == "__main__":
+  n = 5
+  printTable(n)
